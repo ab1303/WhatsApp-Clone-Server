9fdf0cacfa8b88d0922c09aa06757fe99316f8ed
shahiab <abdul.shahid@ofx.com>
2020-08-07 15:18:19 +1000

    typesafety

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -1,15 +1,17 @@
 import { DateTimeResolver, URLResolver } from 'graphql-scalars';
-import { chats, messages } from '../db';
+import { Message, chats, messages } from '../db';
+import { Resolvers } from '../types/graphql';
 
-const resolvers = {
+const resolvers: Resolvers = {
   Date: DateTimeResolver,
   URL: URLResolver,
   Chat: {
-    lastMessage(chat: any) {
+    lastMessage(chat) {
       const lastMessageId = chat.messages[chat.messages.length - 1];
-      return messages.find((m) => m.id === lastMessageId);
+
+      return messages.find((m) => m.id === lastMessageId) || null;
     },
-    messages(chat: any) {
+    messages(chat) {
       return messages.filter((m) => chat.messages.includes(m.id));
     },
   },
@@ -18,8 +20,36 @@ const resolvers = {
     chats() {
       return chats;
     },
-    chat(root: any, { chatId }: any) {
-      return chats.find((c) => c.id === chatId);
+    chat(root, { chatId }) {
+      return chats.find((c) => c.id === chatId) || null;
+    },
+  },
+
+  Mutation: {
+    addMessage(root: any, { chatId, content }: any) {
+      const chatIndex = chats.findIndex((c) => c.id === chatId);
+
+      if (chatIndex === -1) return null;
+
+      const chat = chats[chatIndex];
+
+      const messagesIds = messages.map((currentMessage) =>
+        Number(currentMessage.id)
+      );
+      const messageId = String(Math.max(...messagesIds) + 1);
+      const message = {
+        id: messageId,
+        createdAt: new Date(),
+        content,
+      };
+
+      messages.push(message);
+      chat.messages.push(messageId);
+      // The chat will appear at the top of the ChatsList component
+      chats.splice(chatIndex, 1);
+      chats.unshift(chat);
+
+      return message;
     },
   },
 };
