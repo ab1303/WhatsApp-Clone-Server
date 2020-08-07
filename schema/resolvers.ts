ec2e4c2373d31953a905bf0d4db8f52ff338653b
shahiab <abdul.shahid@ofx.com>
2020-08-07 16:15:47 +1000

    Live updates - graphql Subscriptions

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -5,14 +5,24 @@ import { Resolvers } from '../types/graphql';
 const resolvers: Resolvers = {
   Date: DateTimeResolver,
   URL: URLResolver,
-  Chat: {
-    lastMessage(chat) {
-      const lastMessageId = chat.messages[chat.messages.length - 1];
 
-      return messages.find((m) => m.id === lastMessageId) || null;
+  Message: {
+    chat(message) {
+      return (
+        chats.find((c) => c.messages.some((m) => m === message.id)) || null
+      );
     },
+  },
+
+  Chat: {
     messages(chat) {
       return messages.filter((m) => chat.messages.includes(m.id));
+    },
+
+    lastMessage(chat) {
+      const lastMessage = chat.messages[chat.messages.length - 1];
+
+      return messages.find((m) => m.id === lastMessage) || null;
     },
   },
 
@@ -20,13 +30,14 @@ const resolvers: Resolvers = {
     chats() {
       return chats;
     },
+
     chat(root, { chatId }) {
       return chats.find((c) => c.id === chatId) || null;
     },
   },
 
   Mutation: {
-    addMessage(root: any, { chatId, content }: any) {
+    addMessage(root, { chatId, content }, { pubsub }) {
       const chatIndex = chats.findIndex((c) => c.id === chatId);
 
       if (chatIndex === -1) return null;
@@ -37,7 +48,7 @@ const resolvers: Resolvers = {
         Number(currentMessage.id)
       );
       const messageId = String(Math.max(...messagesIds) + 1);
-      const message = {
+      const message: Message = {
         id: messageId,
         createdAt: new Date(),
         content,
@@ -49,7 +60,18 @@ const resolvers: Resolvers = {
       chats.splice(chatIndex, 1);
       chats.unshift(chat);
 
+      pubsub.publish('messageAdded', {
+        messageAdded: message,
+      });
+
       return message;
+    },
+  },
+
+  Subscription: {
+    messageAdded: {
+      subscribe: (root, args, { pubsub }) =>
+        pubsub.asyncIterator('messageAdded'),
     },
   },
 };
